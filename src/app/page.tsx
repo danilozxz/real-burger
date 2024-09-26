760d03fba859d2f6a0fc279a6f225ce833e1ca25
Danilozxz <progdanilo@gmail.com>
2024-09-26 15:56:13 -0300

    refactor: modificação na estrutura do header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,24 @@ export default function Home() {
     <div className="mx-auto text-white">
       <header className="flex justify-between items-center px-4 xl:px-80 pt-8">
 
-        <div className="flex items-center gap-28">
-          <p className="text-4xl">Real Burger</p>
+        <div className="flex items-center gap-28 w-full">
+          <div>
+            <a href="#" className="text-4xl">Real <span className="text-[#FE6A0F]">•</span> Burguer</a>
+          </div>
 
-          <ul className="hidden xl:flex gap-16">
-            <li className="hover:text-[#FE6A0F] transition-all"><a href="#">Início</a></li>
-            <li className="hover:text-[#FE6A0F] transition-all"><a href="#">Bugers</a></li>
-            <li className="hover:text-[#FE6A0F] transition-all"><a href="#">Contato</a></li>
-          </ul>
+          <nav className="flex flex-1 items-center">
+            <ul className="hidden xl:flex flex-1 gap-16">
+              <li className="hover:text-[#FE6A0F] transition-all"><a href="#">Início</a></li>
+              <li className="hover:text-[#FE6A0F] transition-all"><a href="#">Bugers</a></li>
+              <li className="hover:text-[#FE6A0F] transition-all"><a href="#">Contato</a></li>
+            </ul>
+            <ShoppingCart className="hidden xl:block size-8 cursor-pointer hover:text-[#FE6A0F] transition-all" />
+          </nav>
+
         </div>
 
-        <ShoppingCart className="hidden xl:block size-8 cursor-pointer hover:text-[#FE6A0F] transition-all" />
         <Menu className="size-12 xl:hidden cursor-pointer hover:text-[#FE6A0F] transition-all" />
+
       </header>
     </div>
   );
