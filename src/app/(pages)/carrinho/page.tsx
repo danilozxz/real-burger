fd14617d20f8c75833746ddb45ee0a643b817de0
Danilozxz <progdanilo@gmail.com>
2024-11-11 15:31:43 -0300

    feat: add aviso ao carrinho

--- src/app/(pages)/carrinho/page.tsx
+++ src/app/(pages)/carrinho/page.tsx
@@ -2,8 +2,9 @@ const Page = () => {
     return (
         <div className="px-4 lg:px-40 xl:px-60 pt-8">
             <h1 className="text-3xl xl:text-4xl my-12 leading-[74px] font-bold text-center">Seu carrinho</h1>
-            <div className="bg-[#272727] p-8 md:p-12 lg:p-32 rounded-lg">
-                <a href="/pagamento">Continuar</a>
+            <div className="bg-[#272727] p-8 md:p-12 rounded-lg flex flex-col items-center">
+                <h2 className="mb-12 text-2xl">Seu carrinho está vazio</h2>
+                <a href="/pagamento" className="mt-4 bg-[#FA6A0F] text-white py-2 px-4 rounded">Continuar</a>
             </div>
         </div>
     );
